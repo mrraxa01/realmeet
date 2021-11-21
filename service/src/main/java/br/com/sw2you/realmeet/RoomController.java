f82f6a37a1c5c6e507bf0d1fb637c3191c4700aa
mrraxa01 <mrraxa01@gmail.com>
2021-11-21 17:52:35 -0300

    Criando classe Room

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -1,16 +1,21 @@
 package br.com.sw2you.realmeet;
 
+
+import java.util.concurrent.CompletableFuture;
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
 import br.com.sw2you.realmeet.api.model.Room;
-import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RoomController implements RoomsApi {
+public class RoomController implements
+        RoomsApi {
 
     @Override
     public CompletableFuture<ResponseEntity<Room>> listRooms(Long id) {
         return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room#1")));
     }
+
+
 }
